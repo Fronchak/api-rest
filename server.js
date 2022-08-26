ea6f62fa14bff379da2b4b921cbcdeb072d613a6
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-08-25 21:08:07 -0300

    Criado requisição para a pagina inicial

---
+++ server.js
@@ -0,0 +1,7 @@
+import app from "./app";
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening request from port ${port}`);
+  console.log(`Click here: http://localhost:${port}`);
+});
