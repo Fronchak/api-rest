90b9249cedade6f35c37077db10e1864ab81c9fa
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-08-28 21:55:01 -0300

    Criado o upload de arquivos usando multer

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import alunoController from "../controllers/AlunoController";
+import loginRequired from "../middlewares/loginRequired";
 
 const router = new Router();
 router.get("/", alunoController.index);
-router.post("/", alunoController.store);
-router.delete("/:id", alunoController.delete);
-router.put("/:id", alunoController.update);
+router.post("/", loginRequired, alunoController.store);
+router.delete("/:id", loginRequired, loginRequired, alunoController.delete);
+router.put("/:id", loginRequired, alunoController.update);
 router.get("/:id", alunoController.show);
 
 export default router;
